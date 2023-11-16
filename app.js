a12b3adb1be4386fd2ff70e5fe9151136d01d990
noahbei <nbeidelman9@gmail.com>
2023-11-16 10:32:21 -0800

    add account-data post test

--- app.js
+++ app.js
@@ -6,7 +6,18 @@ const port = 3000
 
 
 app.get("/", (req, res) => {
-    res.send("server");
+    res.send("server running");
+})
+
+app.post("/account-data", async (req, res) => {
+    try {
+        const result = await database.createAccount('2', 'jane', 'password', '13', 'this2@email.com', '2010-12-03', 'female', 'jane doe')
+        console.log(result);
+        res.send(result)
+    } catch (error) {
+        console.error("Error posting account data: " + error)
+        res.send("Error")
+    }
 })
 
 // app.get("/data", async (req, res) => {
